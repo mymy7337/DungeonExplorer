9d2365189b80723f5b4f4a8f4a3b694260a19bc5
mymy7337 <minyeong7337@gmail.com>
2025-08-08 17:04:09 +0900

    [Feat] PlayerManager 추가

---
+++ Assets/02. Scripts/PlayerManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    private static PlayerManager instance;
+    public static PlayerManager Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = new GameObject("PlayerManager").GetComponent<PlayerManager>();
+            }
+            return instance;
+        }
+    }
+
+    private Player player;
+    public Player Player
+    {
+        get { return player; }
+        set { player = value; }
+    }
+
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            if(instance != this)
+                Destroy(gameObject);
+        }
+    }
+}
