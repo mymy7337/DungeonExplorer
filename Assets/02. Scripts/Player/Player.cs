aa27ebab52396589e806cf34e03e47adb903615c
mymy7337 <minyeong7337@gmail.com>
2025-08-11 11:48:48 +0900

    [Feat] Player 컨디션 추가

--- Assets/02. Scripts/Player/Player.cs
+++ Assets/02. Scripts/Player/Player.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    PlayerController controller;
+    public PlayerController controller;
+    public PlayerCondition condition;
 
     private void Awake()
     {
         PlayerManager.Instance.Player = this;
         controller = GetComponent<PlayerController>();
+        condition = GetComponent<PlayerCondition>();
     }
 }
--- Assets/02. Scripts/Player/PlayerCondition.cs
+++ Assets/02. Scripts/Player/PlayerCondition.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerConditions : MonoBehaviour
+public class PlayerCondition : MonoBehaviour
 {
     public UICondition uiCondition;
 
