9d5b7f780634fac98b88969408e0547b45b38a98
mymy7337 <minyeong7337@gmail.com>
2025-08-11 15:56:38 +0900

    [Feat] 아이템오브젝트 추가

---
+++ Assets/02. Scripts/Item/ItemObject.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IInteractable
+{
+    public string GetInteractPrompt();
+    public void OnInteract();
+}
+
+public class ItemObject : MonoBehaviour, IInteractable
+{
+    public ItemDataSO data;
+
+    public string GetInteractPrompt()
+    {
+        string str = $"{data.displayName}\n{data.description}";
+        return str;
+    }
+
+    public void OnInteract()
+    {
+        PlayerManager.Instance.Player.itemData = data;
+        PlayerManager.Instance.Player.addItem?.Invoke();
+        Destroy(gameObject);
+    }
+}
