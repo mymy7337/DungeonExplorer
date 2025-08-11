72d0c87999f6fe7455d8391e744256c2419017fc
mymy7337 <minyeong7337@gmail.com>
2025-08-11 09:14:51 +0900

    [Feat] Player 이동 추가

--- Assets/02. Scripts/Player.cs
+++ Assets/02. Scripts/Player.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     PlayerController controller;
+    Rigidbody _rigidbody;
 
     private void Awake()
     {
--- Assets/02. Scripts/PlayerController.cs
+++ Assets/02. Scripts/PlayerController.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Movement")]
+    public float moveSpeed;
+    private Vector2 curMovementInput;
+    public float jumpForce;
+    public LayerMask groundLayerMask;
+
+    Rigidbody _rigidbody;
+
+    private void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        Vector3 dir = transform.forward * curMovementInput.y + transform.right * curMovementInput.x;
+        dir *= moveSpeed;
+        dir.y = _rigidbody.velocity.y;
+
+        _rigidbody.velocity = dir;
+    }
+
+    public void OnMoveInput(InputAction.CallbackContext context)
+    {
+        Debug.Log("호출");
+        if(context.phase == InputActionPhase.Started)
+        {
+            Debug.Log("스타트");
+            curMovementInput = context.ReadValue<Vector2>();
+        }
+        else if(context.phase == InputActionPhase.Performed)
+        {
+            Debug.Log("퍼폼드");
+        }
+        else if (context.phase == InputActionPhase.Canceled)
+        {
+            curMovementInput = Vector2.zero;
+        }
     }
 }
