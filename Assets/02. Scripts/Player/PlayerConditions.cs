7db476b4310f0b997a29ba3446c97482b192a559
mymy7337 <minyeong7337@gmail.com>
2025-08-11 11:33:34 +0900

    [Feat] PlayerCondition 추가

---
+++ Assets/02. Scripts/Player/PlayerConditions.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerConditions : MonoBehaviour
+{
+    public UICondition uiCondition;
+
+    private Condition health { get { return uiCondition.health; } }
+    private Condition hunger { get { return uiCondition.hunger; } }
+    private Condition stamina { get { return uiCondition.stamina; } }
+
+    public float noHungerHealthDecay;
+
+    public event Action OnTakeDamage;
+
+    void Update()
+    {
+        hunger.Subtract(hunger.passiveValue * Time.deltaTime);
+        stamina.Add(stamina.passiveValue * Time.deltaTime);
+
+        if(hunger.curValue < 0f)
+        {
+            health.Subtract(noHungerHealthDecay * Time.deltaTime);
+        }
+
+        if(health.curValue < 0f)
+        {
+            Die();
+        }
+    }
+
+    public void Heal(float amount)
+    {
+        health.Add(amount);
+    }
+
+    public void Eat(float amount)
+    {
+        hunger.Add(amount);
+    }
+
+    public void Die()
+    {
+        Debug.Log("»ç¸Á");
+    }
+}
