97fea89c821a7c153ce39774758ae4404a0b445b
mymy7337 <minyeong7337@gmail.com>
2025-08-12 10:09:26 +0900

    [Feat] 스태미너 감소 추가

--- Assets/02. Scripts/Player/PlayerCondition.cs
+++ Assets/02. Scripts/Player/PlayerCondition.cs
@@ -57,4 +57,15 @@ public class PlayerCondition : MonoBehaviour, IDamagable
         health.Subtract(damageAmount);
         OnTakeDamage?.Invoke();
     }
+
+    public bool UseStamina(float amount)
+    {
+        if(stamina.curValue - amount < 0)
+        {
+            return false;
+        }
+
+        stamina.Subtract(amount);
+        return true;
+    }
 }
